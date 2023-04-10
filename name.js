83c1d6dc06bdb4d45a0f5e6be41c40e84f6ec401
mdmukhtarmohsin <mdmukhtarmohsin@gmail.com>
2023-04-10 22:57:05 +0530

    firstcommit

---
+++ name.js
@@ -0,0 +1,6 @@
+let name = "Mukhtar";
+console.log(name);
+name = "Mohsin";
+console.log(name);
+name = "Mohsina";
+console.log(name);
---
+++ problem4.js
@@ -0,0 +1,3 @@
+let name = "Mukhtar", school = "Masai", grade = "A", roll_no = 28, subject1 = 10 , subject2 = 10, subject3 = 10;
+console.log("  Name  |  School  |Grade|Roll_No|Subject1|Subject2|Subject3|");
+console.log(" "+name+"|  "+school+"   |  "+grade+"  |  "+roll_no+"   |   "+subject1+"    |  "+subject2+"   |   "+subject3+"   |");
